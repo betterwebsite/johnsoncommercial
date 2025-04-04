196208655e82711520dcc2e2ba5738f3af3a8913
betterwebsite <normanwebdesigners@gmail.com>
2025-04-04 14:21:30 -0500

    oh push
    Jordan Babb

---
+++ src/_data/client.js
@@ -0,0 +1,21 @@
+module.exports = {
+    "name": "Johnson Commercial Group",
+    "email": "help@codestitch.app",
+    "phoneForTel": "405-435-3572",
+    "phoneFormatted": "(405) 435-3572",
+    "address": {
+        "lineOne": "615 24th Ave SW",
+        "lineTwo": "Suite B",
+        "city": "Norman",
+        "state": "OK",
+        "zip": "73069",
+        "country": "US",
+        "mapLink": "https://maps.app.goo.gl/TEdS5KoLC9ZcULuQ6"
+    },
+    "socials": {
+        "facebook": "https://www.facebook.com/JohnsonCommercialGroup?mibextid=LQQJ4d",
+        "instagram": "https://www.instagram.com/johnson.commercial.group/",
+    },
+    //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
+    "domain": "https://www.johnsoncommercialgroup.com/"
+};
