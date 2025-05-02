d76ad6031c933d9e4e52f5f1bbc8a6c39ca7b063
betterwebsite <normanwebdesigners@gmail.com>
2025-05-02 14:47:16 -0500

    another one
    JB

--- src/utils/sanity-client.js
+++ src/utils/sanity-client.js
@@ -1,12 +1,15 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
 const { createClient } = require('@sanity/client');
 
-const token = process.env.SANITY_READ_TOKEN;
 const client = createClient({
   projectId: 'lgrua1a8',
   dataset: 'production',
   apiVersion: '2023-05-03',
-  useCdn: !token,
-  token,
+  useCdn: true,
+  token: process.env.SANITY_READ_TOKEN,
 });
 
 module.exports = client;
